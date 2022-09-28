f01d32cf09638e62bbb17742e54dca7c08fb76c5
元彬 蒋 <2956606223@qq.com>
2022-09-28 16:33:27 +0800

    数据的加减运算

--- MyApp/src/main/java/com/bin/NumberOperate.java
+++ MyApp/src/main/java/com/bin/NumberOperate.java
@@ -8,14 +8,36 @@ public class NumberOperate {
     static int y = 0;  //分母
 
     public static void main(String[] args) {
-        getXY("12/34");
-        System.out.println(x);
-        System.out.println(y);
+        System.out.println(add("1","1/3"));
     }
 
     //两个数相加
     public static String add(String one, String two) {
-        return null;
+        getXY(one);
+        int oneX = x;
+        int oneY = y;
+        getXY(two);
+        int twoX = x;
+        int twoY = y;
+        x = oneX*twoY + oneY*twoX;
+        y = oneY * twoY;
+        return simplify(x,y);
+    }
+
+    //两个数相减
+    public static String subtract(String one, String two) {
+        getXY(one);
+        int oneX = x;
+        int oneY = y;
+        getXY(two);
+        int twoX = x;
+        int twoY = y;
+        x = oneX*twoY - oneY*twoX;
+        if (x<0){
+            return null; //如果分子出现负数返回null
+        }
+        y = oneY * twoY;
+        return simplify(x,y);
     }
 
     //提取数据的分子和分母
@@ -23,7 +45,6 @@ public class NumberOperate {
         //恢复xy的初始值
         x = 0;
         y = 0;
-
         int index = 0;
         int n = number.length();
         while (index < n) {//读取分子大小
@@ -32,7 +53,7 @@ public class NumberOperate {
                 index++;
             }
             index++;  //跳过符号/的索引位置,进入分母的索引
-            if(index == n){
+            if(index >= n){
                 y=1;
             }
             // 读取分母
@@ -43,4 +64,26 @@ public class NumberOperate {
         }
     }
 
+    //化简分数
+    public static String  simplify(int a,int b){
+        int gcd = gcd(a, b);
+        a /= gcd;
+        b /= gcd;
+        if (b == 1){
+            return  ""+a;
+        }else {
+            return a+"/"+b;
+        }
+    }
+
+    //计算分子分母最大公约数
+    public static int gcd(int a, int b) {
+        int remainder = a % b;
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            remainder = a % b;
+        }
+        return b;
+    }
 }
